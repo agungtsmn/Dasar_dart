86d6ca93cb11f3dc0ef9f0965516fabe052ff7a3
agungtsmn <0812agung@gmail.com>
2021-03-16 10:47:48 +0700

    Percabangan

---
+++ dasar_dart/bin/percabangan.dart
@@ -0,0 +1,23 @@
+
+// Amanda Agung Permata
+// 6304191217
+// Pengembangan Aplikasi Mobile Multiplatform
+
+// ------------------------------------------
+// Perulangan
+
+import 'dart:io';
+main(List<String> args) {
+  print("Masukkan Angka");
+  var number = int.tryParse(stdin.readLineSync());
+
+  if(number > 100){
+    print("Nilai yang anda masukkan lebih besar dari 100");
+  }
+  else if(number < 100){
+    print("Nilai yang anda masukkan lebih kecil dari 100");
+  }
+  else{
+    print("Nilai yang anda masukkan sama dengan 100");
+  }
+}
