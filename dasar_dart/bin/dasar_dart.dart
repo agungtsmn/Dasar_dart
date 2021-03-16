539b280ef59412856c7690e39c174edaa292144a
agungtsmn <0812agung@gmail.com>
2021-03-16 10:10:12 +0700

    Dasar (New Project)

---
+++ dasar_dart/bin/dasar_dart.dart
@@ -0,0 +1,42 @@
+
+// Amanda Agung Permata
+// 6304191217
+// Pengembangan Aplikasi Mobile Multiplatform
+
+// -------------------------------------------
+// Menampilkan "Hello World"
+
+// import 'dart:io';
+// void main(List<String> arguments) {
+//   print("Hello World");
+// }
+
+// --------------------------------------------
+// Membuat inputan dan menampilkan
+
+// import 'dart:io';
+// void main(List<String> arguments) {
+//   var input = stdin.readLineSync();
+//   var number = int.tryParse(input);
+//   print(number);
+// }
+
+// --------------------------------------------
+// Membuat inputan dan ditambahkan 20
+
+// import 'dart:io';
+// void main(List<String> arguments) {
+//   var input = stdin.readLineSync();
+//   var number = int.tryParse(input);
+//   print(number + 20);
+// }
+
+// --------------------------------------------
+// Membuat inputan bertipe double dan dikalikan 5
+
+// import 'dart:io';
+// void main(List<String> arguments) {
+//   var input = stdin.readLineSync();
+//   var number = double.tryParse(input);
+//   print(number * 5);
+// }
