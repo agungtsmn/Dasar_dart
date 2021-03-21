6ca0e6eb6a2e8119e6c98c87be5da66a4b40cca2
agungtsmn <0812agung@gmail.com>
2021-03-21 16:32:10 +0700

    Perulangan

---
+++ dasar_dart/bin/perulangan.dart
@@ -0,0 +1,51 @@
+// Amanda Agung Permata
+// 6304191217
+// Pengembangan Aplikasi Mobile Multiplatform
+
+// ------------------------------------------
+// Perulangan FOR
+
+// import 'dart:io';
+// main(List<String> args) {
+//   for(int ulang = 0; ulang < 5; ulang = ulang + 1){
+//     print("halo ke $ulang");
+//   }
+//   print(" ");
+//   for(int ulang = 1; ulang <= 5; ulang = ulang + 1){
+//     print("halo ke $ulang");
+//   }
+//   print(" ");
+//   for(int ulang = 5; ulang > 0; ulang = ulang - 1){
+//     print("halo ke $ulang");
+//   }
+//   print(" ");
+//   for(int ulang = 1; ulang <= 5; ulang += 1){
+//     print("halo ke $ulang");
+//   }
+// }
+
+// ------------------------------------------
+// Perulangan WHILE
+
+// import 'dart:io';
+// main(List<String> args) {
+//   int bil = 1;
+//   while(bil <= 10){
+//     print("hallo ke $bil");
+//     bil += 1;
+//   }
+// }
+
+// ------------------------------------------
+// Perulangan WHILE
+
+// import 'dart:io';
+// main(List<String> args) {
+//   var bil = 1;
+//   do {
+//     print('Hello ke $bil');
+//     bil ++;
+//   } while (bil <= 5);
+
+//   print(" ");
+// }
